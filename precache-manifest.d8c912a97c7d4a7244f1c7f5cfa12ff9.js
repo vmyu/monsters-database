eb9a4e959471b649d74ad8952e347be25781eaf0
vmyu <vince.m.yu@gmail.com>
2020-09-13 21:42:22 -0400

    Updates

---
+++ precache-manifest.d8c912a97c7d4a7244f1c7f5cfa12ff9.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "846ae63788b38fe6e954064bd85760c7",
+    "url": "/monsters-database/index.html"
+  },
+  {
+    "revision": "16cd734c2a02e4a740e6",
+    "url": "/monsters-database/static/css/main.92597cc9.chunk.css"
+  },
+  {
+    "revision": "0ac27ccecaca5553eea2",
+    "url": "/monsters-database/static/js/2.640f7b8d.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/monsters-database/static/js/2.640f7b8d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "16cd734c2a02e4a740e6",
+    "url": "/monsters-database/static/js/main.517e5683.chunk.js"
+  },
+  {
+    "revision": "8a1e1494a25399e22df7",
+    "url": "/monsters-database/static/js/runtime-main.0a9bf81f.js"
+  }
+]);
